da0179dab22ddc7fb915f3263ec126e3e2963eb9
kuno4 <dmncrtl@example.com>
2022-06-22 12:20:15 -0400

    moved files

---
+++ config.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class config {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("weather.config");
+        Scanner input = new Scanner(file);
+        String latitude = "";
+        String longitude = "";
+        String key = "";
+
+        latitude = input.nextLine();
+        latitude = input.next();
+	    longitude = input.next();
+        key = input.next();
+
+        FileWriter latw = new FileWriter("lat.txt");
+        FileWriter lonw = new FileWriter("lon.txt");
+        FileWriter apiw = new FileWriter("api.txt");
+
+        latw.write(latitude);
+        latw.close();
+
+        lonw.write(longitude);
+        lonw.close();
+
+        apiw.write(key);
+        apiw.close();
+
+        input.close();
+    }
+}
